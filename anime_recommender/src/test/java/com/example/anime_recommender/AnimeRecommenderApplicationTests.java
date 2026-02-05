5724faf85fba292a64d96ddb954530fb1463c277
Jonathan Morgan <jonmorgan1717@gmail.com>
2026-02-05 16:06:38 -0500

    dotenv/workflow error fix

--- anime_recommender/src/test/java/com/example/anime_recommender/AnimeRecommenderApplicationTests.java
+++ anime_recommender/src/test/java/com/example/anime_recommender/AnimeRecommenderApplicationTests.java
@@ -9,17 +9,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.anime_recommender.service.AnimeApiRequests;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 
 @SpringBootTest
 class AnimeRecommenderApplicationTests {
 
 	static {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("Client_ID", dotenv.get("Client_ID"));
-        System.setProperty("spring.datasource.url", dotenv.get("db_url"));
-        System.setProperty("spring.datasource.username", dotenv.get("db_UserName"));
-        System.setProperty("spring.datasource.password", dotenv.get("db_password"));
-	}
+	
+		Dotenv dotenv = null;
+		try {
+			dotenv = Dotenv.load();
+		} catch (DotenvException e) {
+			System.out.println("No .env file found, using environment variables");
+		}
+		if (dotenv != null) {
+			System.setProperty("Client_ID", dotenv.get("Client_ID"));
+			System.setProperty("spring.datasource.url", dotenv.get("db_url"));
+			System.setProperty("spring.datasource.username", dotenv.get("db_UserName"));
+			System.setProperty("spring.datasource.password", dotenv.get("db_password"));
+		}
+	
+			}
 
 	@Autowired
     private AnimeApiRequests animeApiRequests;
