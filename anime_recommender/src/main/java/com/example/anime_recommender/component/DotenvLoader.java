6a27f322af84f18ed91b07ceada41f7c77fecd20
Jonathan Morgan <jonmorgan1717@gmail.com>
2026-02-06 14:21:46 -0500

    remove dotenvloader bean

--- anime_recommender/src/main/java/com/example/anime_recommender/component/DotenvLoader.java
+++ anime_recommender/src/main/java/com/example/anime_recommender/component/DotenvLoader.java
@@ -1,19 +1,19 @@
-package com.example.anime_recommender.component;
-import io.github.cdimascio.dotenv.Dotenv;
+// package com.example.anime_recommender.component;
+// import io.github.cdimascio.dotenv.Dotenv;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
 
-import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class DotenvLoader {
+// @Component
+// public class DotenvLoader {
 
-    @PostConstruct
-    public void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
+//     @PostConstruct
+//     public void loadEnv() {
+//         Dotenv dotenv = Dotenv.load();
 
-        System.setProperty("db_url", dotenv.get("db_url"));
-        System.setProperty("db_UserName", dotenv.get("db_UserName"));
-        System.setProperty("db_password", dotenv.get("db_password"));
-    }
-}
+//         System.setProperty("db_url", dotenv.get("db_url"));
+//         System.setProperty("db_UserName", dotenv.get("db_UserName"));
+//         System.setProperty("db_password", dotenv.get("db_password"));
+//     }
+// }
