46dbf1efc72f88656da05e32aa6e067d12de9235
Jonathan Morgan <jonmorgan1717@gmail.com>
2026-03-24 17:03:48 -0400

    Fix: Testing if this variable is messing with axios

--- anime_frontend/src/api/animeApi.js
+++ anime_frontend/src/api/animeApi.js
@@ -2,24 +2,22 @@ import axios from "axios";
 
 //used for accessing front end endpoints
 
-const url = process.env.REACT_APP_API_URL || ""
-
 export async function getSeasonalAnime() {
-  const response = await axios.get(`${url}/api/anime/seasonal/weekly`);
+  const response = await axios.get(`/api/anime/seasonal/weekly`);
   return response; 
 }
 
 export async function getAnimeByID(id) {
-  const response = await axios.get(`${url}/api/anime/${id}`);
+  const response = await axios.get(`/api/anime/${id}`);
   return response;
 }
 
 export async function getGenres() {
-  const response = await axios.get(`${url}/api/anime/seasonal/genre`);
+  const response = await axios.get(`/api/anime/seasonal/genre`);
   return response;
 }
 export async function getAnimeByGenre(genre, page, size) {
-  const response = await axios.get(`${url}/api/anime/seasonal/genre/${genre}`, {params: {
+  const response = await axios.get(`/api/anime/seasonal/genre/${genre}`, {params: {
     page: page,
     size: size
   }});
@@ -27,17 +25,17 @@ export async function getAnimeByGenre(genre, page, size) {
 }
 
 export async function getPopularPage() {
-  const response = await axios.get(`${url}/api/anime/seasonal/popular/preview`);
+  const response = await axios.get(`/api/anime/seasonal/popular/preview`);
   return response;
 }
 
 export async function getDiscoverPopular(page, size) {
   var response;
   if (page === 0){
-    response = axios.get(`${url}/api/anime/seasonal/discover`)
+    response = axios.get(`/api/anime/seasonal/discover`)
   }
   else{
-    response = axios.get(`${url}/api/anime/seasonal/popular`, {params: {
+    response = axios.get(`/api/anime/seasonal/popular`, {params: {
       page: page,
       size: size
     }})
